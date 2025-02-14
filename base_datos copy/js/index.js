696c3d8692d6de32731e05cba376805f7ceec96f
tonilogardev <tologardev@gmail.com>
2025-02-14 15:03:24 +0100

    hasta ejercicio 11

--- base_datos copy/js/index.js
+++
@@ -1,8 +0,0 @@
-function toggleIndice() {
-  var lista = document.getElementById("listaEjercicios");
-  if (lista.style.display === "none" || lista.style.display === "") {
-    lista.style.display = "block";
-  } else {
-    lista.style.display = "none";
-  }
-}
